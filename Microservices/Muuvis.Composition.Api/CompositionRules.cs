e84982faa5dc5a8722448aef2b1742ca32e0f9e0
Cristian Civera <cristian@cristiancivera.com>
2018-01-29 01:20:55 +0100

    Composition

---
+++ Microservices/Muuvis.Composition.Api/CompositionRules.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Routing.Template;
+
+namespace Muuvis.Composition.Api
+{
+    public class CompositionRules
+    {
+        public void Add(string sourceTemplate, string routeUrlTemplate)
+        {
+            RouteTemplate sourceRouteTemplate = TemplateParser.Parse(sourceTemplate);
+        }
+    }
+}
---
+++ Microservices/Muuvis.Composition.Api/Startup.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.Routing.Template;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Muuvis.Composition.Api
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //app.UseRouter(r => r.MapAreaRoute())
+            var compositionRules = new CompositionRules();
+            compositionRules.Add("movies/{id}-{a}", "http://muuvis.catalog.api/movies/{id}");
+            app.UseMiddleware<CompositionMiddleware>(compositionRules);
+            //TemplateParser
+            //new TemplateMatcher().
+        }
+    }
+}
